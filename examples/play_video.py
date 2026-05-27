1e9f4c08f6af564b94a5661bbbd209d28d29e991
Sam Lavigne <splavigne@gmail.com>
2026-05-27 15:23:56 -0400

    update play video example

--- examples/play_video.py
+++ examples/play_video.py
@@ -2,16 +2,24 @@ from p5skia import Canvas
 
 c = Canvas(show=True, fps=30)
 
-vid = c.load_video("myvideo.mp4")
+# load a video
+vid = c.load_video("./examples/testvideo.mp4")
+
+# loop it
 vid.loop = True
 
+# resize the canvas to make it the same dimensions of the video
 c.resize(vid.width, vid.height)
 
 while c.draw():
     c.background(1, 1, 1)
 
     scale = 1
+
+    # display a frame from the video
     c.image(vid.image, 0, 0, c.width, c.height)
+
+    # display multiple
     while scale > 0.1:
         scale -= 0.1
         c.push()
@@ -19,4 +27,6 @@ while c.draw():
         c.scale(scale)
         c.image(vid.image, -vid.width / 2, -vid.height / 2)
         c.pop()
+
+    # go to the next frame
     vid.next()
